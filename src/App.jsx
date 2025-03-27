c69de5ee798e39ca40459c0a2999050aac545d47
Rafaa <rafaabennour14@gmail.com>
2025-03-27 00:10:23 +0000

    Add Axios for API calls and implement Article and Articles components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css';
+import Article from './components/Article';
+import Articles from './components/Articles';
+import HeaderBar from './components/HeaderBar';
+
 
 function App() {
   return (
    <div>
+    <HeaderBar/>
+    <Articles/>
     
    </div>
   );
