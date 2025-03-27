8179ac7fa5c4ad66d5ca75f67b651e426fb24b32
Rafaa <rafaabennour14@gmail.com>
2025-03-27 21:01:35 +0000

    Add Home and Login components with basic structure and functionality

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeaderBar from "../components/HeaderBar";
+import Articles from "../components/Articles";
+
+export default function Home() {
+  return (
+    <div>
+        
+        <HeaderBar/>
+        <Articles/>
+        
+       </div>
+  )
+}
